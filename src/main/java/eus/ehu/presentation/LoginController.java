f3303c602287e2429cd82aa89ca1588fc7e144fd
LMThen <lthen001@ikasle.ehu.eus>
2026-02-10 11:18:39 +0100

    Theme selector - Work in Progress

--- src/main/java/eus/ehu/presentation/LoginController.java
+++ src/main/java/eus/ehu/presentation/LoginController.java
@@ -6,6 +6,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.Scene;
 
 public class LoginController {
 
@@ -13,13 +15,17 @@ public class LoginController {
     private ComboBox<String> roleCombo;
     @FXML
     private Label roleLabel;
+    @FXML
+    private RadioButton darkTheme;
+    @FXML
+    private RadioButton lightTheme;
 
     private ObservableList<String> roles;
 
     @FXML
     void comboHandler(ActionEvent event) {
         String role = roleCombo.getValue();
-        roleLabel.setText("You selected: " + role);
+        roleLabel.setText(role);
     }
 
     @FXML
@@ -30,6 +36,18 @@ public class LoginController {
         
         // bind the roleLabel text property to the selected role value property
         roleLabel.textProperty().bind(roleCombo.valueProperty());
+    }
+
+    @FXML
+    void switchTheme(){
+        Scene scene = roleCombo.getScene();
+        scene.getStylesheets().clear();
+
+        if (darkTheme.isSelected()) {
+            scene.getStylesheets().add("https://raw.githubusercontent.com/antoniopelusi/JavaFX-Dark-Theme/main/style.css");
+        } else if (lightTheme.isSelected()) {
+            scene.getStylesheets().clear();
+        }
     }
 
 
